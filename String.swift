8b9f2a416142b86838504c73c29d0d429de7fd09
Satyam Sehgal <satyams407@outlook.com>
2019-04-15 11:36:33 +0530

    Validate palindrome or not

--- String.swift
+++ String.swift
@@ -2,8 +2,31 @@
 print("Enter the string")
 let string = readLine()!
 var resultString = ""
-for i in 1...string.count {
-    let index = string.index(string.endIndex, offsetBy: -i)
+for i in 0...(string.count - 1)  {
+    let index = string.index(string.startIndex, offsetBy: (string.count - 1) - i)
     resultString.append(string[index])
 }
 print(resultString)
+
+
+// MARK: Validate a string palindrome or not
+func testPalindrome(palindromeStr: String) -> Bool {
+    var left = 0
+    var right = palindromeStr.count - 1
+    if palindromeStr.count == 0 {
+         return false
+    } else if palindromeStr.count == 1 {
+        return true
+    }
+        
+    while left < right {
+      let leftIndex = palindromeStr.index(palindromeStr.startIndex, offsetBy: left)
+      let rightIndex = palindromeStr.index(palindromeStr.startIndex, offsetBy: right)
+      if palindromeStr[leftIndex] !=  palindromeStr[rightIndex] {
+          return false
+      }
+      left = left + 1
+      right = right - 1
+    }
+   return true
+}
