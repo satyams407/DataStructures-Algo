ab40c1a1687df0481c6ff03d11db86230f19358d
Satyam Sehgal <satyams407@outlook.com>
2021-05-28 23:53:17 +0530

    Kth largest element

--- Array.swift
+++ Array.swift
@@ -241,3 +241,26 @@ func maxiumSumSubArray(arr: [Int], k: Int) -> Int? {
         }
         return maxSum
     }
+
+ // Time Complexity: O(nlogn), Space Complexity: O(n)
+ func findKthLargest(_ nums: [Int], _ k: Int) -> Int {
+        guard let pivot = nums.first else {
+            fatalError("Invalid Input")
+        }
+       
+        let leftPart = nums.filter { $0 > pivot }
+        let middlePart = nums.filter { $0 == pivot}
+        let rightPart = nums.filter { $0 < pivot }
+        
+        if nums.count == middlePart.count {
+            return pivot
+        }
+        
+        if leftPart.count > k - 1 {
+            return findKthLargest(leftPart, k)
+        } else if k - leftPart.count <= middlePart.count {
+            return findKthLargest(middlePart, k - leftPart.count)
+        } else {
+            return findKthLargest(rightPart, k - leftPart.count - middlePart.count)
+        }
+    }
