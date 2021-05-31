93840a971befcb8fdb51e4c9493c4c07c2f95624
Satyam Sehgal <satyams407@outlook.com>
2021-05-31 23:53:39 +0530

    [Array]TwoSum

---
+++ Array/TwoSum.swift
@@ -0,0 +1,30 @@
+func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
+    guard numbers.count > 1 else { return [Int]() }
+    
+    var left = 0, right = numbers.count - 1
+    
+    while left < right {
+        if numbers[left] + numbers[right] < target {
+            left += 1
+        } else if numbers[left] + numbers[right] > target {
+            right -= 1
+        } else {
+            return [left + 1, right + 1]
+        }
+    }
+    
+    return [Int]()
+}
+
+// return two indexes which sum up to target K (TWO SUM)
+func twoSumII(_ nums: [Int], _ target: Int) -> [Int] {
+    var dict = Dictionary<Int, Int>()
+    
+    for (key, value) in nums.enumerated() {
+        if let idx = dict[target - value] {
+            return [idx, key]
+        }
+        dict[value] = key
+    }
+    fatalError("no pair found")
+}
